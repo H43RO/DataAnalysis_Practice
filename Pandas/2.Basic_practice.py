6ae20b0d7f2dec7b357a5d6fc91cffec8ee20950
H43RO <h2is1234@naver.com>
2020-09-04 14:00:00 +0900

    Pandas Operation

--- Pandas/2.Basic_practice.py
+++ Pandas/2.Basic_practice.py
@@ -1,2 +1,34 @@
 import pandas as pd
+import numpy as np
 
+# Data Frame 의 Null 여부 판단
+word_dict = {
+    'Apple': '사과',
+    'Banana': '바나나',
+    'Carrot': '당근',
+    'Durian': '두리안'
+}
+
+frequency_dict = {
+    'Apple': 3,
+    'Banana': 5,
+    'Carrot': np.nan,
+    'Durian': 2
+}
+
+importance_dict = {
+    'Apple': 3,
+    'Banana': 2,
+    'Carrot': 1,
+    'Durian': 1
+}
+
+word = pd.Series(word_dict)
+frequency = pd.Series(frequency_dict)
+importance = pd.Series(importance_dict)
+
+summary = pd.DataFrame({
+    'word': word,
+    'frequency': frequency,
+    'importance': importance
+})
